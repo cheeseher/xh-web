d6b1408d44d00dbba570cb9d490fc1ba5fd24c27
Shane Sheng <shanesheng@example.com>
2025-03-11 22:24:52 +0700

    fix: 简化配置以修复构建错误

--- next.config.js
+++ next.config.js
@@ -15,56 +15,6 @@ const nextConfig = {
   },
   productionBrowserSourceMaps: false,
   output: 'export',
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      // 生产环境客户端配置
-      config.optimization = {
-        ...config.optimization,
-        minimize: true,
-        minimizer: [
-          '...',
-          new (require('css-minimizer-webpack-plugin'))(),
-        ],
-        splitChunks: {
-          chunks: 'all',
-          minSize: 10000,
-          maxSize: 20000,
-          minChunks: 1,
-          maxAsyncRequests: 30,
-          maxInitialRequests: 30,
-          enforceSizeThreshold: 20000,
-          cacheGroups: {
-            defaultVendors: false,
-            framework: {
-              chunks: 'all',
-              name: 'framework',
-              test: /(?<!node_modules.*)[\\/]node_modules[\\/](react|react-dom|scheduler|prop-types|use-subscription)[\\/]/,
-              priority: 40,
-              enforce: true,
-            },
-            commons: {
-              name: 'commons',
-              minChunks: 3,
-              priority: 20,
-            },
-            lib: {
-              test: /[\\/]node_modules[\\/]/,
-              priority: 10,
-              reuseExistingChunk: true,
-            },
-            default: false,
-            styles: {
-              name: 'styles',
-              test: /\.(css|scss)$/,
-              chunks: 'all',
-              enforce: true,
-            },
-          },
-        },
-      }
-    }
-    return config
-  },
 }
 
 module.exports = nextConfig 
