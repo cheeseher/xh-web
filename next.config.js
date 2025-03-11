f50b941fda6b9092de70e15f6d2e1a09bfb3de42
Shane Sheng <shanesheng@example.com>
2025-03-11 22:11:28 +0700

    perf: 优化构建输出文件大小

--- next.config.js
+++ next.config.js
@@ -3,8 +3,17 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   distDir: 'out',
+  compress: true,
   images: {
     domains: ['via.placeholder.com'],
+    minimumCacheTTL: 60,
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+  experimental: {
+    optimizeCss: true,
+    optimizePackageImports: ['@mui/icons-material', '@mui/material'],
   },
 }
 
